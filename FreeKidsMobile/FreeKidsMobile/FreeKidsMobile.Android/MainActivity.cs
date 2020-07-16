34615f753e166ebcfe67af85d7db3c30a8f61215
IsmailAyvaz <64161610+IsmailAyvaz@users.noreply.github.com>
2020-07-16 20:47:23 +0300

    Android WebView INTERNET Permission
    Adapting WebView to Android

--- FreeKidsMobile/FreeKidsMobile/FreeKidsMobile.Android/MainActivity.cs
+++ FreeKidsMobile/FreeKidsMobile/FreeKidsMobile.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace FreeKidsMobile.Droid
 {
-    [Activity(Label = "FreeKidsMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FreeKidsMobile", Icon = "@drawable/logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
